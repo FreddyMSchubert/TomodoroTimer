a5c857570bbecbbf42f5933e58e581f66de3b7c7
SelfindulgentNonsense <github@f.s6t.de>
2023-10-08 19:37:26 +0200

    v0.4.0 - Added saving of settings in local storage

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,4 @@
+/* ----- POPUP ----- */
 const popup = document.getElementById('popup');
 
 document.addEventListener('click', (event) => {
@@ -7,7 +8,7 @@ document.addEventListener('click', (event) => {
 });
 
 
-
+/* ----- SETTINGS ----- */
 function updateTimes()
 {
     const workDurationInput = document.getElementById('workDurationInput');
@@ -37,9 +38,72 @@ function updateAutoContinue()
   startWorkAutomatically = startWorkAutomaticallyUpdated;
 }
 
-document.getElementById('workDurationInput').addEventListener('input', updateTimes);
-document.getElementById('breakDurationInput').addEventListener('input', updateTimes);
-document.getElementById('foregroundColorInput').addEventListener('input', updateColors);
-document.getElementById('backgroundColorInput').addEventListener('input', updateColors);
-document.getElementById('startWorkAutomatically').addEventListener('input', updateAutoContinue);
-document.getElementById('startBreakAutomatically').addEventListener('input', updateAutoContinue);
+
+/* ----- LOCAL STORAGE SAVE ----- */
+function saveSettings() {
+  localStorage.setItem('workTime', workTime);
+  localStorage.setItem('breakTime', breakTime);
+  localStorage.setItem('foregroundColor', document.documentElement.style.getPropertyValue('--second-color'));
+  localStorage.setItem('backgroundColor', document.documentElement.style.getPropertyValue('--first-color'));
+  localStorage.setItem('startWorkAutomatically', startWorkAutomatically);
+  localStorage.setItem('startBreakAutomatically', startBreakAutomatically);
+}
+
+function loadSettings() {
+  const savedWorkTime = localStorage.getItem('workTime');
+  const savedBreakTime = localStorage.getItem('breakTime');
+  const savedForegroundColor = localStorage.getItem('foregroundColor');
+  const savedBackgroundColor = localStorage.getItem('backgroundColor');
+  const savedStartWorkAutomatically = localStorage.getItem('startWorkAutomatically') === 'true';
+  const savedStartBreakAutomatically = localStorage.getItem('startBreakAutomatically') === 'true';
+
+  if (savedWorkTime) document.getElementById('workDurationInput').value = savedWorkTime / 60;
+  if (savedBreakTime) document.getElementById('breakDurationInput').value = savedBreakTime / 60;
+  if (savedForegroundColor) document.getElementById('foregroundColorInput').value = savedForegroundColor;
+  if (savedBackgroundColor) document.getElementById('backgroundColorInput').value = savedBackgroundColor;
+  if (savedStartWorkAutomatically) document.getElementById('startWorkAutomatically').checked = savedStartWorkAutomatically;
+  if (savedStartBreakAutomatically) document.getElementById('startBreakAutomatically').checked = savedStartBreakAutomatically;
+
+  // Call the update functions to apply the loaded settings
+  updateTimes();
+  updateColors();
+  updateAutoContinue();
+}
+
+document.addEventListener('DOMContentLoaded', loadSettings);
+
+/* ----- LISTENERS ----- */
+document.getElementById('workDurationInput').addEventListener('input', () => { updateTimes(); saveSettings(); });
+document.getElementById('breakDurationInput').addEventListener('input', () => { updateTimes(); saveSettings(); });
+document.getElementById('foregroundColorInput').addEventListener('input', () => { updateColors(); saveSettings(); });
+document.getElementById('backgroundColorInput').addEventListener('input', () => { updateColors(); saveSettings(); });
+document.getElementById('startWorkAutomatically').addEventListener('input', () => { updateAutoContinue(); saveSettings(); });
+document.getElementById('startBreakAutomatically').addEventListener('input', () => { updateAutoContinue(); saveSettings(); });
+
+/* ---- SETTINGS RESET ----- */
+
+const defaultSettings = {
+  workTime: 25 * 60,
+  breakTime: 5 * 60,
+  foregroundColor: '#00ff00',
+  backgroundColor: '#000000',
+  startWorkAutomatically: false,
+  startBreakAutomatically: true
+};
+
+function resetToDefault() {
+  document.getElementById('workDurationInput').value = defaultSettings.workTime / 60;
+  document.getElementById('breakDurationInput').value = defaultSettings.breakTime / 60;
+  document.getElementById('foregroundColorInput').value = defaultSettings.foregroundColor;
+  document.getElementById('backgroundColorInput').value = defaultSettings.backgroundColor;
+  document.getElementById('startWorkAutomatically').checked = defaultSettings.startWorkAutomatically;
+  document.getElementById('startBreakAutomatically').checked = defaultSettings.startBreakAutomatically;
+
+  updateTimes();
+  updateColors();
+  updateAutoContinue();
+
+  saveSettings();
+}
+
+document.getElementById('resetButton').addEventListener('click', resetToDefault);
